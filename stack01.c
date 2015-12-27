76894da361fcdc081d0b1c2c7065b8c4dd24c6a2
ChristopherDiehl <csd92@scarletmail.rutgers.edu>
2015-12-27 11:33:37 -0500

    C

--- stack01.c
+++ stack01.c
@@ -3,9 +3,12 @@
 
 void function(int a, int b) {
 	int array[5];
+	printf ("%d\n",array[6]);
+
 }
 
-main() {
+int main(void) {
 	function(1,2);
-	printf("This is where the return address points");
+	printf("This is where the return address points \n");
 }
+//to get assembly language code: gcc -S -c foo.c
