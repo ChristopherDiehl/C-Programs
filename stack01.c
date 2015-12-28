ad9aa4b62e0dcfa84bde05f1a240fbae3220d881
ChristopherDiehl <csd92@scarletmail.rutgers.edu>
2015-12-28 16:46:00 -0500

    Fixed problem with sizeof(array) thought it return length of array, didn't realize that it was data size in memory

--- stack01.c
+++ stack01.c
@@ -1,22 +1,23 @@
 #include <stdio.h>
 #include <string.h>
+void function(int a, int b);
+
+
+main() {
+	function(1,2);
+	printf("This is where the return address points \n");
+}
 
 void function(int a, int b) {
 	int array[5];
 	int i ;
-	printf("Size of array: %ld",sizeof(array));
-	for(i=0; i<sizeof(array); i ++){
+	for(i=0; i < 5; i ++){
 		array[i] = i;
 		printf("%d\n" , array[i]);
 
 	}
 	//printf("%d\n" , array[6]);
 
-}
-
-main() {
-	function(1,2);
-	printf("This is where the return address points \n");
 }
 /*to get assembly language code: gcc -S -c foo.c
 //to compile to executable (only for single source) use :
