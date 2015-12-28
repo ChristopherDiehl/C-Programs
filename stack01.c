479511fe05599eb72b0ed5c0e029bd889b672b8d
ChristopherDiehl <csd92@scarletmail.rutgers.edu>
2015-12-28 16:38:17 -0500

    Some notes added for future c projects

--- stack01.c
+++ stack01.c
@@ -3,12 +3,24 @@
 
 void function(int a, int b) {
 	int array[5];
-	printf ("%d\n",array[6]);
+	int i ;
+	printf("Size of array: %ld",sizeof(array));
+	for(i=0; i<sizeof(array); i ++){
+		array[i] = i;
+		printf("%d\n" , array[i]);
+
+	}
+	//printf("%d\n" , array[6]);
 
 }
 
-int main(void) {
+main() {
 	function(1,2);
 	printf("This is where the return address points \n");
 }
-//to get assembly language code: gcc -S -c foo.c
+/*to get assembly language code: gcc -S -c foo.c
+//to compile to executable (only for single source) use :
+//gcc stack01.c -o stack*/
+
+
+
